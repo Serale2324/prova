28cbb5263f8fb8339943ce8ae9ddbceaf6ce9321
camarda <101434222+camarda@users.noreply.github.com>
2023-10-24 21:37:55 +0200

    aggiunta file doc

---
+++ doc.js
@@ -0,0 +1,11 @@
+
+/**
+ * cosa fa la funzione
+ * @param {int} uno valore iniziale
+ * @param {string} due informazione generica
+ * @return ritorna sempre 0
+ */
+function documentazione(uno, due){
+    //........
+    return 0;
+}
